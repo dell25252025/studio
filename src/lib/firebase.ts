7c46034cbee8ee52b7801e35b88da92db0250430
dell dell <dell25252025@gmail.com>
2025-09-07 10:40:48 +0000

    je veux lier mon application a la base de donnée firebase et voila mes c

---
+++ src/lib/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApps, getApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCGC-3H86sqqgigM2H5bIE4e1bEmGnKJz0",
+  authDomain: "wanderlink-c1a35.firebaseapp.com",
+  projectId: "wanderlink-c1a35",
+  storageBucket: "wanderlink-c1a35.appspot.com",
+  messagingSenderId: "186522309970",
+  appId: "1:186522309970:web:85d430fa8caa742a1b968b",
+  measurementId: "G-BSPSX9RKVL"
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+export default app;
