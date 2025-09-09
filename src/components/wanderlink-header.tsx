4c1b30d0dd9f870aadd871ba05ea8d34fe4701ae
dell dell <dell25252025@gmail.com>
2025-09-09 16:38:06 +0000

    vous laisser le teime comme il est juste ajuter le titre pour qu'il soit

--- src/components/wanderlink-header.tsx
+++ src/components/wanderlink-header.tsx
@@ -4,9 +4,9 @@ import { cn } from '@/lib/utils';
 
 const Logo = () => (
   <svg 
-    width="150" 
+    width="135" 
     height="40" 
-    viewBox="0 0 150 40" 
+    viewBox="0 0 135 40" 
     xmlns="http://www.w3.org/2000/svg" 
     className="h-8 w-auto"
     aria-label="travel.me Logo"
@@ -26,9 +26,9 @@ const Logo = () => (
     </defs>
     
     <text x="0" y="28" className="logo-text">travel</text>
-    <text x="115" y="28" className="logo-text">me</text>
+    <text x="96" y="28" className="logo-text">me</text>
     
-    <g transform="translate(87, 14) scale(0.9)">
+    <g transform="translate(73, 14) scale(0.9)">
         <path d="M 10 0 L 12 8 L 10 10 L 8 8 Z" fill="hsl(var(--accent))" />
         <path d="M 20 10 L 12 12 L 10 10 L 12 8 Z" fill="hsl(var(--accent))" opacity="0.7"/>
         <path d="M 10 20 L 8 12 L 10 10 L 12 12 Z" fill="hsl(var(--primary))" />
