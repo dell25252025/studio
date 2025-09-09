df5a4cf77179b7193246c34e717ef67bec573f9e
dell dell <dell25252025@gmail.com>
2025-09-09 15:10:22 +0000

    je veux le me soit plus prof et bien vu sur le titre la on le voit pas

--- src/components/wanderlink-header.tsx
+++ src/components/wanderlink-header.tsx
@@ -4,11 +4,11 @@ import { cn } from '@/lib/utils';
 
 const Logo = () => (
   <svg 
-    width="250" 
-    height="100" 
-    viewBox="0 0 280 100" 
+    width="280" 
+    height="60" 
+    viewBox="0 0 280 60" 
     xmlns="http://www.w3.org/2000/svg" 
-    className="h-12 w-auto"
+    className="h-10 w-auto"
     aria-label="travel.w.me Logo"
   >
     <defs>
@@ -18,25 +18,21 @@ const Logo = () => (
           .logo-text {
             font-family: 'Poppins', sans-serif;
             font-weight: 500;
-            font-size: 50px;
+            font-size: 48px;
             fill: hsl(var(--foreground));
           }
         `}
       </style>
     </defs>
-
-    {/* Text */}
-    <text x="0" y="65" className="logo-text">travel</text>
-    <text x="210" y="65" className="logo-text">.me</text>
     
-    {/* Stylized W Icon */}
-    <g transform="translate(150, 42) scale(1.4)">
-      <g>
+    <text x="0" y="45" className="logo-text">travel</text>
+    <text x="188" y="45" className="logo-text">.me</text>
+    
+    <g transform="translate(158, 23) scale(1.4)">
         <path d="M 10 0 L 12 8 L 10 10 L 8 8 Z" fill="hsl(var(--accent))" />
         <path d="M 20 10 L 12 12 L 10 10 L 12 8 Z" fill="hsl(var(--accent))" opacity="0.7"/>
         <path d="M 10 20 L 8 12 L 10 10 L 12 12 Z" fill="hsl(var(--primary))" />
         <path d="M 0 10 L 8 12 L 10 10 L 8 8 Z" fill="hsl(var(--primary))" opacity="0.7"/>
-      </g>
     </g>
   </svg>
 );
