f2fdf16866c052b00e970c746ff2087eb5090318
dell dell <dell25252025@gmail.com>
2025-09-10 15:42:26 +0000

    je veux sur la page profil supprimer l'espace entre les photo de profil

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -366,7 +366,7 @@ export default function ProfilePage() {
     <div className="flex min-h-screen w-full flex-col bg-secondary/30">
         <WanderlinkHeader />
         <main className="flex-1 pb-24 pt-10">
-             <div className="w-full bg-background pt-8">
+             <div className="w-full bg-background">
                 {profilePictures.length > 0 ? (
                     <Dialog>
                         <Carousel
