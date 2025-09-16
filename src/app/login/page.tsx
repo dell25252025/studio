a56045c4ec21a7d00dcd30de7dd474765b98ad19
dell dell <dell25252025@gmail.com>
2025-09-16 11:35:47 +0000

    je veux supperime l'espace entre le texte ' nous ne publisions jamais ..

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -169,7 +169,7 @@ export default function AuthPage() {
                             </p>
                         </div>
 
-                        <div className={`flex flex-col gap-4 ${isEmailFormVisible ? 'hidden' : 'block'} mb-4 md:mt-0 mt-[16vh]`}>
+                        <div className={`flex flex-col gap-4 ${isEmailFormVisible ? 'hidden' : 'block'} mb-4 md:mt-0 mt-[14vh]`}>
                             <div className="text-center">
                                 <Button variant="outline" className="w-full bg-white border-slate-200 text-slate-600 hover:bg-slate-50 hover:text-slate-800" onClick={handleGoogleSignIn} disabled={isGoogleLoading || isLoading}>
                                     {isGoogleLoading ? (<Loader2 className="mr-2 h-4 w-4 animate-spin" />) : (
@@ -183,7 +183,7 @@ export default function AuthPage() {
                                     )}
                                     Continuer avec Google
                                 </Button>
-                                <p className="mt-1 text-[9px] text-white md:hidden">
+                                <p className="mt-0.5 text-[9px] text-white md:hidden">
                                     Nous ne publions jamais rien sur vos comptes de réseaux sociaux
                                 </p>
                             </div>
@@ -194,7 +194,7 @@ export default function AuthPage() {
                                 <Button variant="outline" size="icon" aria-label="S'inscrire avec un e-mail" onClick={() => { setIsEmailFormVisible(true); setIsLogin(false); form.reset(); }} className="bg-white border-white text-black hover:bg-slate-100">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect x="2" y="4" width="20" height="16" rx="2"></rect><path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path></svg>
                                 </Button>
-                                <Button variant="link" className="text-white h-auto p-0 text-sm mt-[14vh] mb-2" onClick={() => { setIsEmailFormVisible(true); setIsLogin(true); form.reset(); }}>
+                                <Button variant="link" className="text-white h-auto p-0 text-sm mt-[10vh] mb-2" onClick={() => { setIsEmailFormVisible(true); setIsLogin(true); form.reset(); }}>
                                     Connexion
                                 </Button>
                             </div>
