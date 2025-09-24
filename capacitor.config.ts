0b1563d2d248c08ebbe490bd96d4314a75dc3232
dell dell <dell25252025@gmail.com>
2025-09-24 12:46:25 +0000

    j'ai testé la mise a jour de l'application il y a encore le problème de

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.wanderlink.app.v3',
-  appName: 'WanderLink+',
+  appName: 'WanderLink',
   webDir: 'out'
 };
 
