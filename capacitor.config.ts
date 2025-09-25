bda99da58f1fa8de63c19d31dde10e7c7eb657ed
dell dell <dell25252025@gmail.com>
2025-09-25 11:01:51 +0000

    j ai sync après j'avais plus d'erreur et après j'ai fait play sur mon sm

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,11 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.wanderlink.app.v3',
   appName: 'WanderLink',
-  webDir: 'out'
+  webDir: 'out',
+  server: {
+    url: 'http://192.168.1.3:3000', // Remplacez par votre adresse IP locale
+    cleartext: true
+  }
 };
 
 export default config;
