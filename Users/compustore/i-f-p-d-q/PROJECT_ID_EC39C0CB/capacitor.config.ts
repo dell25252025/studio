928147b63e948be77bff01a2fe4f052de8fae16e
dell dell <dell25252025@gmail.com>
2025-09-26 17:05:16 +0000

    l'image reste blanche sur android studio semulateur

--- Users/compustore/i-f-p-d-q/PROJECT_ID_EC39C0CB/capacitor.config.ts
+++ Users/compustore/i-f-p-d-q/PROJECT_ID_EC39C0CB/capacitor.config.ts
@@ -3,7 +3,6 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.wanderlink.app',
   appName: 'WanderLink',
-  webDir: 'out',
   server: {
     // IMPORTANT: Remplacez par votre véritable adresse IP locale
     url: 'http://REMPLACER_PAR_VOTRE_IP:3000', 
