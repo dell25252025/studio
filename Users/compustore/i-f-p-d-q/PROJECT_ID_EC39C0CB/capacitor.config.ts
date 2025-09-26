0eac54ba69a3a980e6ce35b866fbe67dc2671099
dell dell <dell25252025@gmail.com>
2025-09-26 16:36:36 +0000

    il y a une erreur je pense que j'ai jamais fait "npm run android:open "

--- Users/compustore/i-f-p-d-q/PROJECT_ID_EC39C0CB/capacitor.config.ts
+++ Users/compustore/i-f-p-d-q/PROJECT_ID_EC39C0CB/capacitor.config.ts
@@ -3,12 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.wanderlink.app',
   appName: 'WanderLink',
-  webDir: 'out',
-  server: {
-    // IMPORTANT: Replace with your actual local IP address
-    url: 'http://192.168.1.99:3000', 
-    cleartext: true
-  }
+  webDir: 'out'
 };
 
 export default config;
