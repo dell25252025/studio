397c37f384408e4ecfe9513f6575d69aa97428b0
dell dell <dell25252025@gmail.com>
2025-09-26 17:35:50 +0000

    j'ai toujour image blanche sur android studio et ici ça marche très bien

--- Users/compustore/i-f-p-d-q/PROJECT_ID_EC39C0CB/capacitor.config.ts
+++ Users/compustore/i-f-p-d-q/PROJECT_ID_EC39C0CB/capacitor.config.ts
@@ -4,11 +4,6 @@ const config: CapacitorConfig = {
   appId: 'com.wanderlink.app',
   appName: 'WanderLink',
   webDir: 'out',
-  server: {
-    // IMPORTANT: Remplacez par votre véritable adresse IP locale si elle a changé
-    url: 'http://192.168.1.99:3000', 
-    cleartext: true
-  }
 };
 
 export default config;
