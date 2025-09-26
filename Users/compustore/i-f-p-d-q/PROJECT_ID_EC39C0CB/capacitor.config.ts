302212827cf6b35ab7a21f2f92841196d8029242
dell dell <dell25252025@gmail.com>
2025-09-26 17:18:42 +0000

    a android studio toujours image blanche

--- Users/compustore/i-f-p-d-q/PROJECT_ID_EC39C0CB/capacitor.config.ts
+++ Users/compustore/i-f-p-d-q/PROJECT_ID_EC39C0CB/capacitor.config.ts
@@ -3,9 +3,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.wanderlink.app',
   appName: 'WanderLink',
+  webDir: 'out',
   server: {
-    // IMPORTANT: Remplacez par votre véritable adresse IP locale
-    url: 'http://REMPLACER_PAR_VOTRE_IP:3000', 
+    // IMPORTANT: Remplacez par votre véritable adresse IP locale si elle a changé
+    url: 'http://192.168.1.99:3000', 
     cleartext: true
   }
 };
