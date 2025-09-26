f05d4607a72f264d45640ae03334f3eca7b6955d
dell dell <dell25252025@gmail.com>
2025-09-26 18:07:34 +0000

    ca reste comme ça je fais quoi ?

--- Users/compustore/i-f-p-d-q/PROJECT_ID_EC39C0CB/capacitor.config.ts
+++ Users/compustore/i-f-p-d-q/PROJECT_ID_EC39C0CB/capacitor.config.ts
@@ -3,11 +3,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.wanderlink.app',
   appName: 'WanderLink',
-  webDir: 'out',
   server: {
     // This is the special IP address that the Android emulator uses
     // to connect to the host machine (your computer).
-    url: 'http://10.0.2.2:3000', 
+    url: 'http://10.0.2.2:3005', 
     cleartext: true
   }
 };
