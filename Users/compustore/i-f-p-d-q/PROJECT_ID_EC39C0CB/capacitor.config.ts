a6f8d1d1ee138b12b4daf7b0bb652a4a80095d89
dell dell <dell25252025@gmail.com>
2025-09-26 17:43:24 +0000

    image touche blanche sur simulateur de android studio

--- Users/compustore/i-f-p-d-q/PROJECT_ID_EC39C0CB/capacitor.config.ts
+++ Users/compustore/i-f-p-d-q/PROJECT_ID_EC39C0CB/capacitor.config.ts
@@ -4,6 +4,12 @@ const config: CapacitorConfig = {
   appId: 'com.wanderlink.app',
   appName: 'WanderLink',
   webDir: 'out',
+  server: {
+    // This is the special IP address that the Android emulator uses
+    // to connect to the host machine (your computer).
+    url: 'http://10.0.2.2:3000', 
+    cleartext: true
+  }
 };
 
 export default config;
