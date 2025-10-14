1ca1acd96614f8c56e34e4cd38a831d218f5bd1e
dell dell <dell25252025@gmail.com>
2025-10-14 17:50:09 +0000

    la j'ai changé de lieu c'est pas le meme wifi ou j'étais avant avec le q

--- Users/compustore/i-f-p-d-q/PROJECT_ID_EC39C0CB/capacitor.config.ts
+++ Users/compustore/i-f-p-d-q/PROJECT_ID_EC39C0CB/capacitor.config.ts
@@ -6,7 +6,7 @@ const config: CapacitorConfig = {
   server: {
     // IMPORTANT: Remplacez par l'adresse IP locale ACTUELLE de votre ordinateur.
     // Assurez-vous que votre téléphone est sur le même réseau WiFi.
-    url: 'http://192.168.1.99:3000', 
+    url: 'http://VOTRE_NOUVELLE_ADRESSE_IP:3000', 
     cleartext: true
   }
 };
