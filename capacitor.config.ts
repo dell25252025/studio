a6214bfc70d2b22b11aa093ec8ce666b32a66599
dell dell <dell25252025@gmail.com>
2025-10-14 17:52:01 +0000

    voila la nouvelle adresse ip

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,10 +3,8 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.wanderlink.app',
   appName: 'WanderLink',
-  webDir: 'out',
   server: {
-    // IMPORTANT: Replace with your actual local IP address
-    url: 'http://192.168.1.99:3000', 
+    url: 'http://192.168.56.1:3000', 
     cleartext: true
   }
 };
