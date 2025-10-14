b6a21d951d3e16297f5863fa5966320503a926ed
dell dell <dell25252025@gmail.com>
2025-10-14 18:08:00 +0000

    voila après : npm run dev

--- Users/compustore/i-f-p-d-q/PROJECT_ID_EC39C0CB/capacitor.config.ts
+++ Users/compustore/i-f-p-d-q/PROJECT_ID_EC39C0CB/capacitor.config.ts
@@ -4,9 +4,7 @@ const config: CapacitorConfig = {
   appId: 'com.wanderlink.app',
   appName: 'WanderLink',
   server: {
-    // IMPORTANT: Remplacez par l'adresse IP locale ACTUELLE de votre ordinateur.
-    // Assurez-vous que votre téléphone est sur le même réseau WiFi.
-    url: 'http://VOTRE_NOUVELLE_ADRESSE_IP:3000', 
+    url: 'http://192.168.100.26:3001', 
     cleartext: true
   }
 };
