71afc0107d7f409c7611a684664cfc2ce35b8d7b
dell dell <dell25252025@gmail.com>
2025-10-15 11:32:37 +0000

    le serveur il tourne  comme ça ?

--- Users/compustore/i-f-p-d-q/PROJECT_ID_EC39C0CB/capacitor.config.ts
+++ Users/compustore/i-f-p-d-q/PROJECT_ID_EC39C0CB/capacitor.config.ts
@@ -4,7 +4,7 @@ const config: CapacitorConfig = {
   appId: 'com.wanderlink.app',
   appName: 'WanderLink',
   server: {
-    url: 'http://192.168.100.26:3002', 
+    url: 'http://192.168.100.26:3000', 
     cleartext: true
   }
 };
