dba5878a0c7769c59c5a996845afe9de60fd3e97
dell dell <dell25252025@gmail.com>
2025-10-16 11:23:26 +0000

    meme je fais sync now rien ne change l'erreur reste je dois peut etre fa

--- Users/compustore/i-f-p-d-q/PROJECT_ID_EC39C0CB/capacitor.config.ts
+++ Users/compustore/i-f-p-d-q/PROJECT_ID_EC39C0CB/capacitor.config.ts
@@ -6,6 +6,8 @@ const config: CapacitorConfig = {
   appName: 'WanderLink',
   webDir: 'out',
   server: {
+    // IMPORTANT: Replace with your computer's local IP address.
+    // Find it on Windows with `ipconfig` or on macOS/Linux with `ifconfig`.
     url: 'http://192.168.100.26:3000', 
     cleartext: true
   }
