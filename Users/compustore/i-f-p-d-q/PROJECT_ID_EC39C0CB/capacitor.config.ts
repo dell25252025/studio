2189d5eea069a3df2814ba16e2a40e3a0d225341
dell dell <dell25252025@gmail.com>
2025-10-21 11:48:17 +0000

    toujolurs l'utlisateur quand il veut creer un compte avec le compte goog

--- Users/compustore/i-f-p-d-q/PROJECT_ID_EC39C0CB/capacitor.config.ts
+++ Users/compustore/i-f-p-d-q/PROJECT_ID_EC39C0CB/capacitor.config.ts
@@ -10,7 +10,14 @@ const config: CapacitorConfig = {
     // Find it on Windows with `ipconfig` or on macOS/Linux with `ifconfig`.
     url: 'http://192.168.100.26:3000', 
     cleartext: true
-  }
+  },
+  plugins: {
+    GoogleAuth: {
+      scopes: ['profile', 'email'],
+      serverClientId: '186522309970-13g4a6l03a4p97vj38m3d2tgdne924n0.apps.googleusercontent.com',
+      forceCodeForRefreshToken: true,
+    },
+  },
 };
 
 export default config;
