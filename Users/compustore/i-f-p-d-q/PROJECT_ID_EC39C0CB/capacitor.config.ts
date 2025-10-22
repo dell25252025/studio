539dab90f8fdec4b61d9b3857085e4bfc0226725
dell dell <dell25252025@gmail.com>
2025-10-22 15:41:28 +0000

    le resultat après la commande npx cap sync android

--- Users/compustore/i-f-p-d-q/PROJECT_ID_EC39C0CB/capacitor.config.ts
+++ Users/compustore/i-f-p-d-q/PROJECT_ID_EC39C0CB/capacitor.config.ts
@@ -8,14 +8,7 @@ const config: CapacitorConfig = {
   server: {
     url: 'http://192.168.100.26:3000', 
     cleartext: true
-  },
-  plugins: {
-    GoogleAuth: {
-      scopes: ['profile', 'email'],
-      serverClientId: '186522309970-13g4a6l03a4p97vj38m3d2tgdne924n0.apps.googleusercontent.com',
-      forceCodeForRefreshToken: true,
-    },
-  },
+  }
 };
 
 export default config;
